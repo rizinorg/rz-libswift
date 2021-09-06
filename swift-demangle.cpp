59e20e9a32713d32de6d0c0fe388d6721107b3f4
wargio <wargio@libero.it>
2021-09-06 08:52:12 +0200

    Added SPDX license linter

--- swift-demangle.cpp
+++ swift-demangle.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2021 RizinOrg <info@rizin.re>
+// SPDX-FileCopyrightText: 2021 deroad <wargio@libero.it>
+// SPDX-License-Identifier: LGPL-3.0-only
+
 #include <rz_libswift.h>
 #include <iostream>
 
